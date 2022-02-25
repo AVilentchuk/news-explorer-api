e215c1edbd1f68a1403afc8a0de42061d53193fe
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-25 16:17:10 +0200

    stage-3

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,5 @@
 const Article = require('../models/article');
+const errors = require('../constants/errors');
 
 module.exports.fetchArticles = (req, res, next) => {
   Article.find({})
@@ -8,15 +9,7 @@ module.exports.fetchArticles = (req, res, next) => {
 };
 
 module.exports.newArticle = (req, res) => {
-  const {
-    keyword,
-    title,
-    text,
-    date,
-    source,
-    link,
-    image
-  } = req.body;
+  const { keyword, title, text, date, source, link, image } = req.body;
 
   const owner = req.user._id;
 
@@ -30,14 +23,37 @@ module.exports.newArticle = (req, res) => {
     image,
     owner
   }).then((article) => {
-    Article.findOne(article).then((foundArticle) => res.status(201).send(foundArticle));
+    Article.findOne(article).then((foundArticle) =>
+      res.status(201).send(foundArticle)
+    );
   });
 };
 
+module.exports.deleteArticleByCredentials = (req, res, next) => {
+  const { keyword, title, text, date, source, link, image } = req.body;
+
+  const owner = req.user._id;
+  Article.findOneAndDelete({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner
+  })
+    .then(res.status(200).send({ message: 'Deleted successfully' }))
+    .orFail(() => Promise.reject(errors.articleNotFound))
+    .catch(next);
+};
 module.exports.deleteArticle = (req, res, next) => {
   const articleId = req.params.id;
   Article.checkIfOwner(articleId, req.user._id)
-    .then((item) => Article.deleteOne(item)
-      .then(res.send({ message: 'Deleted successfully' })))
+    .then((item) =>
+      Article.deleteOne(item).then(
+        res.send({ message: 'Deleted successfully' })
+      )
+    )
     .catch(next);
 };
--- routes/articles.js
+++ routes/articles.js
@@ -3,12 +3,14 @@ const { articleValidator, articleAction } = require('../middleware/validator');
 const {
   newArticle,
   fetchArticles,
-  deleteArticle
+  deleteArticle,
+  deleteArticleByCredentials
 } = require('../controllers/articles');
 
 // router.get('/',validator , function);
 router.get('/', fetchArticles);
 router.post('/', articleValidator, newArticle);
 router.delete('/:id', articleAction, deleteArticle);
+router.delete('/', articleValidator, deleteArticleByCredentials);
 
 module.exports = router;
