d5a894ec33bf069eec6e5f10048d7d53e8aef49c
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-26 08:33:57 +0200

    fixed an issue where saved cards will show other users saved cards

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,22 +2,14 @@ const Article = require('../models/article');
 const errors = require('../constants/errors');
 
 module.exports.fetchArticles = (req, res, next) => {
-  Article.find({})
+  Article.find({ owner: req.user._id })
     .populate('owner')
     .then((articles) => res.send({ data: articles }))
     .catch(next);
 };
 
 module.exports.newArticle = (req, res) => {
-  const {
-    keyword,
-    title,
-    text,
-    date,
-    source,
-    link,
-    image
-  } = req.body;
+  const { keyword, title, text, date, source, link, image } = req.body;
 
   const owner = req.user._id;
 
@@ -31,20 +23,14 @@ module.exports.newArticle = (req, res) => {
     image,
     owner
   }).then((article) => {
-    Article.findOne(article).then((foundArticle) => res.status(201).send(foundArticle));
+    Article.findOne(article).then((foundArticle) =>
+      res.status(201).send(foundArticle)
+    );
   });
 };
 
 module.exports.deleteArticleByCredentials = (req, res, next) => {
-  const {
-    keyword,
-    title,
-    text,
-    date,
-    source,
-    link,
-    image
-  } = req.body;
+  const { keyword, title, text, date, source, link, image } = req.body;
 
   const owner = req.user._id;
   Article.findOneAndDelete({
@@ -64,7 +50,10 @@ module.exports.deleteArticleByCredentials = (req, res, next) => {
 module.exports.deleteArticle = (req, res, next) => {
   const articleId = req.params.id;
   Article.checkIfOwner(articleId, req.user._id)
-    .then((item) => Article.deleteOne(item)
-      .then(res.send({ message: 'Deleted successfully' })))
+    .then((item) =>
+      Article.deleteOne(item).then(
+        res.send({ message: 'Deleted successfully' })
+      )
+    )
     .catch(next);
 };
